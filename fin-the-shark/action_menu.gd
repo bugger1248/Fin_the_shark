70eb4ed50bb380fc652086f0098d9e261205e1bc
unknown <bugger1248@github.com>
2026-01-25 19:29:52 +0100

    added actions menu

---
+++ fin-the-shark/action_menu.gd
@@ -0,0 +1,54 @@
+extends Control
+
+enum MENU_DIRS {LEFT, RIGHT, UP, DOWN}
+
+var current_action : int = 0
+var actions_array : Array = ["bite", "dash"]
+
+signal queue_action(action:String)
+
+@onready var fin_actions_container : VBoxContainer = $HBoxContainer/FinActionsContainer
+
+func _ready() -> void:
+	display_chosen_action()
+
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("action_down"):
+		move_front(MENU_DIRS.DOWN)
+	elif Input.is_action_just_pressed("action_up"):
+		move_front(MENU_DIRS.UP)
+	
+	if Input.is_action_just_pressed("choose_action"):
+		choose_action()
+
+func display_chosen_action():
+	var container_size : int = fin_actions_container.get_children().size()
+	for i in range(container_size):
+		if i == current_action:
+			fin_actions_container.get_child(i).set("theme_override_colors/font_shadow_color",Color.BLACK)
+		else:
+			fin_actions_container.get_child(i).set("theme_override_colors/font_shadow_color",Color.TRANSPARENT)
+
+
+func move_front(dir:MENU_DIRS):
+	if dir == MENU_DIRS.UP:
+		if actions_array.size() <= (current_action + 1):
+			current_action = 0
+		else:
+			current_action += 1
+	
+	elif dir == MENU_DIRS.DOWN:
+		if current_action <= 0:
+			current_action = actions_array.size() - 1
+		else:
+			current_action -= 1
+	
+	display_chosen_action()
+
+func choose_action():
+	var chosen_action: String = actions_array[current_action]
+	
+	queue_action.emit(chosen_action)
+	
+	print("this action was chosen : ",chosen_action)
--- fin-the-shark/fin.gd
+++ fin-the-shark/fin.gd
@@ -5,36 +5,29 @@ extends CharacterBody2D
 
 @export var max_speed : int = 100
 
+@export var dash_speed : float = 5
+
+enum CONTROL_STATE {CAN_MOVE, CANNOT_MOVE}
+var current_control_state : int = CONTROL_STATE.CAN_MOVE
+
 var direction_x : int = 0
 var direction_y : int = 0
+
+var dash_final_pos : int = 320
+
+var is_performing : bool = false
 
 func _process(delta: float) -> void:
 
 
 	direction_x = 0
 	direction_y = 0
-
-	if Input.is_action_pressed("move_down"):
-		direction_y = 1
-	if Input.is_action_pressed("move_up"):
-		direction_y = -1
 	
-
-	if Input.is_action_pressed("move_left"):
-		direction_x = -1
-	if Input.is_action_pressed("move_right"):
-		direction_x = 1
+	if current_control_state == CONTROL_STATE.CAN_MOVE:
+		handle_movement(delta)
 
 
-	velocity.x += direction_x * acceleration * delta
-	velocity.y += direction_y * acceleration * delta
-
-	clamp_velocity()
-	
-	
-	apply_friction()
 	position += velocity * delta
-	print(velocity)
 
 func clamp_velocity() -> void:
 	if velocity.x > max_speed:
@@ -71,3 +64,39 @@ func apply_friction() -> void:
 		elif velocity.y < 0:
 			velocity.y += friction
 	
+func execute_dash() -> void:
+	
+	current_control_state = CONTROL_STATE.CANNOT_MOVE
+	
+	
+	
+	var dash_velocity :int = 200
+	velocity = Vector2(dash_velocity,0)
+	
+	var dash_tween : Tween = create_tween()
+	var original_pos : Vector2 = position
+	var final_pos : Vector2 = Vector2(dash_final_pos, position.y)
+	
+	dash_tween.tween_property(self, "position", final_pos, 1 / dash_speed)
+	dash_tween.tween_property(self, "position", original_pos, 1 / dash_speed)
+
+func handle_movement(delta : float) -> void:
+	
+	if Input.is_action_pressed("move_down"):
+		direction_y = 1
+	if Input.is_action_pressed("move_up"):
+		direction_y = -1
+	
+
+	if Input.is_action_pressed("move_left"):
+		direction_x = -1
+	if Input.is_action_pressed("move_right"):
+		direction_x = 1
+
+	velocity.x += direction_x * acceleration * delta
+	velocity.y += direction_y * acceleration * delta
+
+	clamp_velocity()
+	
+	
+	apply_friction()
